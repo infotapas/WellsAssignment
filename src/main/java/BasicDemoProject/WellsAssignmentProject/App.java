31b397ce006300b4640710dbe42e7df506cd958d
Tapas <tapas.padhi@infostretch.com>
2017-06-22 18:32:19 +0530

    code push

---
+++ src/main/java/BasicDemoProject/WellsAssignmentProject/App.java
@@ -0,0 +1,13 @@
+package BasicDemoProject.WellsAssignmentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
